9294f461bb0af6cec8ae1135ad0ef6dd45bee0f2
Albert O'Connor <albert@albertoconnor.ca>
2015-04-22 16:37:50 -0400

    Clean up comments.

--- socializr/contrib/facebooklizr/models.py
+++ socializr/contrib/facebooklizr/models.py
@@ -12,15 +12,11 @@ class FacebookObject(models.Model):
 class FacebookAnalytics(PerDayAnalyticsBase):
     object = models.ForeignKey(FacebookObject)
     # ever increasing
-    # https://graph.facebook.com/v2.3/<page_id>/[likes]
     page_likes = models.PositiveIntegerField(default=0)
 
     # incremental
-    #https://graph.facebook.com/v2.3/100911698621/insights/page_fan_adds_unique
     new_likes = models.PositiveIntegerField(default=0)
-    # https://graph.facebook.com/v2.3/<page_id>/insights/page_posts_impressions
     reach = models.PositiveIntegerField(default=0)
-    # https://graph.facebook.com/v2.3/<page_id>/insights/page_engaged_users/day
     people_engaged = models.PositiveIntegerField(default=0)
 
     class Meta:
